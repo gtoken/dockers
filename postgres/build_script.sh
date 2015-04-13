187859b6f8fbb45c33fee0810ed796cad4e09093
Khang Nguyen <khang@gtoken.com>
2015-04-13 05:21:07 +0000

    Modification with postgres box after getting it to work with this python-box

--- postgres/build_script.sh
+++ postgres/build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir data
+mkdir -p data
 sudo docker stop postgres
 sudo docker rm postgres
 sudo docker build -t gtoken/gtoken-postgres-box .
-sudo docker run -i -t -p 5432 --name postgres -e DB_USER=gtokendb -e PGDATA=/srv/data -v $PWD/data:/srv/data gtoken/gtoken-postgres-box
+sudo docker run -d -p 5432 --name postgres -e DB_USER=gtokendb -e PGDATA=/srv/data -v $PWD/data:/srv/data gtoken/gtoken-postgres-box
