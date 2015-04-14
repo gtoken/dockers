b7163fdfef5e96140aaaaf1e7d79c40b2d2251ac
Khang Nguyen <khang@gtoken.com>
2015-04-14 08:59:59 +0000

    Get the dockerfile from docker instead

--- postgres/build_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p data
-sudo docker stop postgres
-sudo docker rm postgres
-sudo docker build -t gtoken/gtoken-postgres-box .
-sudo docker run -d -p 5432 --name postgres -e DB_USER=gtokendb -e PGDATA=/srv/data -v $PWD/data:/srv/data gtoken/gtoken-postgres-box
